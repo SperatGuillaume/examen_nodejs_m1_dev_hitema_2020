229ee5aedffb4f5ed9ba08cc4a495f1ef2f9c20d
GuillaumeSperat <sperat.guillaume@gmail.com>
2020-05-05 17:06:22 +0200

    all

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -1,9 +1,23 @@
 const crypto = require('crypto');
 
+
 function sha1Encode(data) {
     // To be implemented!
+    let sha1Encode = crypto.createHash('sha1')
+    sha1Encode.update(data)
+    return sha1Encode.digest('hex')
 }
 
 module.exports.digestAuth = (request, response, next) => {
     // To be implemented!
+
+    let authorization = request.headers.authorization;
+    console.log('authorization', authorization);
+
+    let encoded = authorization.replace('Basic', '');
+    let decoded = Buffer.from(encoded, 'base64').toString('utf8');
+    let auth = decoded.split(':');
+    let isValid = auth[0] === 'node' && auth[1] === sha1Encode('password');
+
+    isValid ? next() : response.sendStatus(401);
 }
--- websocket/websocket-server.js
+++ websocket/websocket-server.js
@@ -1,15 +1,23 @@
 const WebSocket = require('ws')
 const webSocketServer = new WebSocket.Server({ port: 3003 });
-  
+
+let history = [];
 webSocketServer.on('connection', webSocket => {
     webSocket.onmessage = messageEvent => {
         const message = messageEvent.data;
+        history.push(message);
         webSocketServer.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message);
             }
         });
     };
+    
+    webSocket.send(JSON.stringify(history));
+
+    // history.map(msg => {
+    //     webSocket.send(msg);
+    // })
 });
 
 module.exports = webSocketServer;
