83bedcfaf2dfed82c8757b5dd20524f223b03c46
GuillaumeSperat <sperat.guillaume@gmail.com>
2020-05-05 17:05:41 +0200

    initial commit

--- api/api.js
+++ api/api.js
@@ -2,8 +2,29 @@ const express = require('express');
 
 const PeopleService = require('./people-service');
 const peopleService = new PeopleService();
+const bodyParser = require('body-parser');
+const v1 = express.Router();
 const app = express();
 
 // To be implemented!
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api/v1', v1);
+
+v1.put('/people/:id', async (request, response) => {
+    let id = request.params.id;
+    let body = request.body;
+    let person = await peopleService.updatePeople(id, body);
+
+    response.send(person);
+});
+
+
+v1.get('/people', async (request, response) => {
+    let filters = request.query;
+
+    const peoples = await peopleService.getPeople(filters);
+    response.send(peoples);
+});
 
 module.exports = app;
--- api/people-service.js
+++ api/people-service.js
@@ -7,9 +7,21 @@ module.exports = class PeopleService {
 
     updatePeople(id, people) {
         // To be implemented!
+        // let people = this.people;
+
+        // let person;
+        // people.map(person => {
+            
+        // })
     }
     
     getPeople(filters) {
         // To be implemented!
+        const filterKeys = Object.keys(filters);
+        return this.peoples.filter(item => {
+            return filterKeys.every(key => {
+                return filters[key] === item[key];
+            });
+        });
     }
 }
